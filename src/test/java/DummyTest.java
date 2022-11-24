4eb8ca7d95b0a8f40ec2c3a36d2579acc666145f
abhaverma <abhaverma90@gmail.com>
2022-11-24 14:39:53 +0530

    Abha | Initial commit with junit5 dependency.

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DummyTest {
+
+    @Test
+    public void dummyTest() {
+        assertEquals(3, 1 + 2);
+    }
+}
